b15c2c458cbd3287bf8b2c978bf5c3209e35cf8f
Lucie Hélias <lucie.helias@protonmail.com>
2024-12-02 11:39:02 +0100

    create Footer component

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,18 @@
+export const Footer = () => {
+  return (
+    <div className="text-white text-center">
+      <p>
+        Challenge by
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          {" "}
+          Frontend Mentor
+        </a>
+        . Coded by{" "}
+        <a href="https://luciehelias.com/" target="_blank">
+          Lucie Helias
+        </a>
+        .
+      </p>
+    </div>
+  );
+};
