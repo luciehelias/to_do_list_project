7f41389f8d2b3c1518071ff98789e63411f43426
Lucie Hélias <lucie.helias@protonmail.com>
2024-12-02 11:42:39 +0100

    create darkMode component

---
+++ src/Components/DarkMode.jsx
@@ -0,0 +1,11 @@
+import iconMoon from "../assets/images/icon-moon.svg";
+import iconSun from "../assets/images/icon-sun.svg";
+
+export const DarkMode = () => {
+  return (
+    <div>
+      <img src={iconMoon} alt="moon dark mode icon " />
+      {/* <img src={iconSun} alt="sun light mode icon " /> */}
+    </div>
+  );
+};
