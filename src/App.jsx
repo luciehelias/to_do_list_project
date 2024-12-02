2417e918db5a3303fb4f3940916d37cee23826b8
Lucie Hélias <lucie.helias@protonmail.com>
2024-12-02 11:45:42 +0100

    implement main App component with structure and imported subcomponents

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { ClearButton } from "./Components/ClearButton";
+import { Footer } from "./Components/Footer";
+import { TaskFilter } from "./Components/TaskFilter";
+import { TaskItem } from "./Components/TaskItem";
+import { TaskList } from "./Components/TaskList";
+import { Header } from "./Components/Header";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
+    <div className="flex flex-col items-center justify-center m-auto bg-black pt-[75px] ">
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Header />
+        <TaskItem placeholder="Create a new todo..." />
+        <TaskList />
+        items left
+        <TaskFilter />
+        <ClearButton />
+        Drag and drop to reorder list
+        <Footer />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default App
+export default App;
