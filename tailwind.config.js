fdd05f6709863660dae0987b084d9a516b38288c
Lucie Hélias <lucie.helias@protonmail.com>
2024-12-02 11:36:06 +0100

    add Tailwind CSS with custom theme configuration and font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['"Josefin Sans"', "sans-serif"], // Définit Josefin Sans comme police par défaut
+      },
+      colors: {
+        light: {
+          veryLightGray: "hsl(0, 0%, 98%)",
+          veryLightGrayishBlue: "hsl(236, 33%, 92%)",
+          lightGrayishBlue: "hsl(233, 11%, 84%)",
+          darkGrayishBlue: "hsl(236, 9%, 61%)",
+          veryDarkGrayishBlue: "hsl(235, 19%, 35%)",
+        },
+        dark: {
+          veryDarkBlue: "hsl(235, 21%, 11%)",
+          veryDarkDesaturatedBlue: "hsl(235, 24%, 19%)",
+          lightGrayishBlue: "hsl(234, 39%, 85%)",
+          lightGrayishBlueHover: "hsl(236, 33%, 92%)",
+          darkGrayishBlue: "hsl(234, 11%, 52%)",
+          veryDarkGrayishBlue1: "hsl(233, 14%, 35%)",
+          veryDarkGrayishBlue2: "hsl(237, 14%, 26%)",
+        },
+      },
+    },
   },
   plugins: [],
 };
