1b4ec3491180936b446f8679d02719bfe59ad14e
Lucie Hélias <lucie.helias@protonmail.com>
2024-12-10 17:53:21 +0100

    update style for the footer component

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,17 +1,20 @@
 export const Footer = () => {
   return (
-    <div className="text-white text-center">
+    <div className="text-dark-darkGrayishBlue text-center mt-14">
       <p>
         Challenge by
         <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
           {" "}
           Frontend Mentor
         </a>
-        . Coded by{" "}
-        <a href="https://luciehelias.com/" target="_blank">
-          Lucie Helias
-        </a>
         .
+        <p>
+          Coded by{" "}
+          <a href="https://luciehelias.com/" target="_blank">
+            Lucie Helias
+          </a>
+          .
+        </p>
       </p>
     </div>
   );
