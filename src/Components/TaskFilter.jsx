4b1654df75ae964dbe60645b5abaaa23a17170b3
Lucie Hélias <lucie.helias@protonmail.com>
2024-12-10 17:52:00 +0100

    Create taskFilter component

--- src/Components/TaskFilter.jsx
+++ src/Components/TaskFilter.jsx
@@ -1,9 +1,67 @@
-export const TaskFilter = () => {
+import { useState } from "react";
+import { useTheme } from "../contexts/theme.context";
+
+export const TaskFilter = ({
+  handleActiveTasks,
+  handleAllTasks,
+  handleCompletedTasks,
+}) => {
+  const [activeCategory, setActiveCategory] = useState("");
+  const { hoverTaskActionColor } = useTheme();
+
   return (
-    <div>
-      <p>All</p>
-      <p>Active</p>
-      <p>Completed</p>
+    <div className="flex gap-4">
+      <p
+        className={` ${hoverTaskActionColor()} cursor-pointer ${
+          activeCategory === "all"
+            ? "text-blue-500"
+            : "text-dark-darkGrayishBlue"
+        }`}
+        onClick={() => {
+          if (activeCategory === "all") {
+            setActiveCategory("");
+          } else {
+            handleAllTasks();
+            setActiveCategory("all");
+          }
+        }}
+      >
+        All
+      </p>
+      <p
+        className={` ${hoverTaskActionColor()} cursor-pointer ${
+          activeCategory === "active"
+            ? "text-blue-500"
+            : "text-dark-darkGrayishBlue"
+        }`}
+        onClick={() => {
+          if (activeCategory === "active") {
+            setActiveCategory("");
+          } else {
+            handleActiveTasks();
+            setActiveCategory("active");
+          }
+        }}
+      >
+        Active
+      </p>
+      <p
+        className={`${hoverTaskActionColor()} cursor-pointer ${
+          activeCategory === "completed"
+            ? "text-blue-500"
+            : "text-dark-darkGrayishBlue"
+        }`}
+        onClick={() => {
+          if (activeCategory === "completed") {
+            setActiveCategory("");
+          } else {
+            handleCompletedTasks();
+            setActiveCategory("completed");
+          }
+        }}
+      >
+        Completed
+      </p>
     </div>
   );
 };
