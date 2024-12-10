7abe463250b59ee40f40e73c438f7298f37e7c99
Lucie Hélias <lucie.helias@protonmail.com>
2024-12-10 17:50:20 +0100

    create theme context for dark mode

---
+++ src/contexts/theme.context.jsx
@@ -0,0 +1,75 @@
+import React, { createContext, useState, useContext } from "react";
+
+const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  // Fonction pour basculer entre les modes clair et sombre
+  const toggleTheme = () => {
+    setIsDarkMode((prevMode) => !prevMode);
+  };
+
+  const backgroundApp = () => {
+    if (isDarkMode) {
+      return "bg-mobile-dark md:bg-desktop-dark bg-dark-veryDarkBlue ";
+    }
+    return "bg-mobile-light md:bg-desktop-light";
+  };
+
+  const borderCircleColor = () => {
+    if (isDarkMode) {
+      return "border-dark-veryDarkGrayishBlue2 border";
+    }
+    return "border-light-lightGrayishBlue border";
+  };
+
+  const backgroundInputColor = () => {
+    if (isDarkMode) {
+      return "bg-dark-veryDarkDesaturatedBlue";
+    }
+    return "bg-white";
+  };
+
+  const borderBottomInputColor = () => {
+    if (isDarkMode) {
+      return "border-b-dark-veryDarkGrayishBlue1 border-b";
+    }
+    return "border-b-light-gray border-b";
+  };
+
+  const backgroundTaskActionColor = () => {
+    if (isDarkMode) {
+      return "bg-dark-veryDarkDesaturatedBlue";
+    }
+    return "bg-white";
+  };
+
+  const hoverTaskActionColor = () => {
+    if (isDarkMode) {
+      return "hover:text-dark-lightGrayishBlue";
+    }
+    return "hover:text-dark-veryDarkGrayishBlue1";
+  };
+
+  return (
+    <ThemeContext.Provider
+      value={{
+        isDarkMode,
+        toggleTheme,
+        borderCircleColor,
+        backgroundInputColor,
+        borderBottomInputColor,
+        backgroundTaskActionColor,
+        hoverTaskActionColor,
+        backgroundApp,
+      }}
+    >
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export const useTheme = () => {
+  return useContext(ThemeContext);
+};
