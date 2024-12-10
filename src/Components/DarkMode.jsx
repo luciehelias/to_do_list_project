ec52efe17ffc4af174100ac97eba396efaa5b4af
Lucie Hélias <lucie.helias@protonmail.com>
2024-12-10 17:53:43 +0100

    update darkMode component

--- src/Components/DarkMode.jsx
+++ src/Components/DarkMode.jsx
@@ -1,11 +1,17 @@
 import iconMoon from "../assets/images/icon-moon.svg";
 import iconSun from "../assets/images/icon-sun.svg";
+import { useTheme } from "../contexts/theme.context";
 
 export const DarkMode = () => {
+  const { isDarkMode, toggleTheme } = useTheme();
+
   return (
     <div>
-      <img src={iconMoon} alt="moon dark mode icon " />
-      {/* <img src={iconSun} alt="sun light mode icon " /> */}
+      {isDarkMode ? (
+        <img src={iconSun} onClick={toggleTheme} alt="sun light mode icon " />
+      ) : (
+        <img src={iconMoon} onClick={toggleTheme} alt="moon dark mode icon " />
+      )}
     </div>
   );
 };
